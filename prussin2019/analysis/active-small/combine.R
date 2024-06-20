13d2456ef3c10cf87c9fe1dea2987e37018050e4
harmonbhasin <62266474+harmonbhasin@users.noreply.github.com>
2024-06-20 13:57:51 -0400

    Tried out osme analysis with new data and it seems good

--- prussin2019/analysis/active-small/combine.R
+++ prussin2019/analysis/active-small/combine.R
@@ -1,13 +1,13 @@
 library(tidyverse)
 
 
-original_metadata <- read_csv('/Users/harmonbhasin/Work/securebio/dataset_analysis/prussin2019/extra_processing/sample-metadata.csv')
-new_metadata <- read_tsv('/Users/harmonbhasin/Work/securebio/dataset_analysis/prussin2019/extra_processing/metadata.tsv', col_name=c('library','na_type','date','sample_alias','season'))
+original_metadata <- read_csv('/Users/harmonbhasin/Work/securebio/nao-harmon/prussin2019/analysis/active-small/sample-metadata.csv')
+new_metadata <- read_tsv('/Users/harmonbhasin/Work/securebio/nao-harmon/prussin2019/analysis/active/extra_processing/metadata.tsv', col_name=c('library','na_type','date','sample_alias','season'))
 
 both <- left_join(original_metadata, new_metadata)
 both
 
-write_csv(both, '/Users/harmonbhasin/Work/securebio/dataset_analysis/prussin2019/new_harmon-sample-metadata.csv')
+write_csv(both, '/Users/harmonbhasin/Work/securebio/nao-harmon/prussin2019/analysis/active-small/harmon-sample-metadata.csv')
 
 
 
