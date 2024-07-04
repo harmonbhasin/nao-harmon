633b010adc8cd3f22c4bc50d1ea13ba13b918a07
harmonbhasin <62266474+harmonbhasin@users.noreply.github.com>
2024-07-04 16:29:01 -0500

    Added everything I did to get the correct files

--- blauwkamp2019/preprocessing/blauwkamp.R
+++ blauwkamp2019/preprocessing/blauwkamp.R
@@ -1,5 +1,23 @@
 library(tidyverse)
 
+# From european bank
 bruh <- read_tsv('~/Downloads/read_experiment_study_links.txt')
 
-bruh %>% filter(str_detect(description, "NextSeq 500 sequencing: DNA Sequencing of Human Plasma: Asymptomatic Subject"))
+asymp <- bruh %>% filter(str_detect(description, "NextSeq 500 sequencing: DNA Sequencing of Human Plasma: Asymptomatic Subject"))
+asymp
+
+
+# NCBI
+us_bruh <- read_csv('~/Downloads/SraRunTable.txt') %>%
+  rename('run_accession'='Run')
+
+
+power_rangers_unite <- left_join(asymp, us_bruh) %>%
+  select(run_accession, everything())
+
+write_csv(power_rangers_unite, '/Users/harmonbhasin/work/securebio/nao-harmon/blauwkamp2019/preprocessing/sraruntable.csv')
+
+remove_non_unique_columns <- function(tbl) {
+  tbl %>%
+    select(where(~n_distinct(.) > 1))
+}
